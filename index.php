2716c425ca0d633595da6d0120b597859d2fcdc4
AselGamer <aselfernandez@gmail.com>
2023-05-10 09:30:43 +0200

    diseño inicial creado

--- index.php
+++ index.php
@@ -4,9 +4,46 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<<<<<<< HEAD
     <title>Document</title>
 </head>
 <body>
 
+=======
+    <link rel="stylesheet" href="css/style.css">
+    <title>Informatikalmi</title>
+</head>
+<body>
+    <div id = "contenido">
+    <h2 id="titulo">Agrega una nueva pregunta</h2>
+    <form>
+      <label for="pregunta">Pregunta:</label>
+      <input type="text" id="pregunta" name="pregunta" required>
+      <label for="respuesta">Respuesta 1:</label>
+      <textarea id="respuesta" name="respuesta" required></textarea>
+      <label for="respuesta">Respuesta 2:</label>
+      <textarea id="respuesta" name="respuesta" required></textarea>
+      <label for="respuesta">Respuesta 3:</label>
+      <textarea id="respuesta" name="respuesta" required></textarea>
+      <label for="respuesta">Respuesta 4:</label>
+      <textarea id="respuesta" name="respuesta" required></textarea>
+      <label for="radios" id="labelRadios">Cual es la correcta:</label>
+      <div id="radio">
+      <label for="pregunta1">Pregunta 1:</label>
+      <input type="radio" id="" name="preguntaCor" value="" required>
+      <label for="pregunta2">Pregunta 2:</label>
+      <input type="radio" id="" name="preguntaCor" value="">
+      <label for="pregunta3">Pregunta 3:</label>
+      <input type="radio" id="" name="preguntaCor" value="">
+      <label for="pregunta4">Pregunta 4:</label>
+      <input type="radio" id="" name="preguntaCor" value="">
+      </div>
+      <input type="submit" value="Agregar pregunta">
+    </form>
+    <footer>
+
+    </footer>
+    </div>
+>>>>>>> f2fa098 (diseño inicial creado)
 </body>
 </html>
