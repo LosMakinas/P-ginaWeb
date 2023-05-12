3bb63666d2229b63a64b2eb9a5c4949d6e238294
LosMakinas <losmakinas27@gmail.com>
2023-05-12 11:43:04 +0200

    preguntas

--- js/preguntas.js
+++ js/preguntas.js
@@ -1,29 +1,85 @@
 $(document).ready(function()
 {
-    $("#contenido").submit(function(event)
+    let boolDropdown = false;
+    let boolTextFiled = false;
+    let boolRadio = false;
+    let boolImg = false;
+    let boolSegs = false;
+
+    $(".pregunta").keyup(function()
     {
+    var pregunta = $(this).val();
+
+    if(pregunta == "")
+    {
+        boolTextFiled = false;
+        
+    }
+
+    if ($("#respuesta1").val() != "" && $("#respuesta2").val() != "" && $("#respuesta3").val() != "" && $("#respuesta4").val() != "") {
+        boolTextFiled = true;
+    }
+
+    console.log(boolTextFiled);
+
+    });
+
+    $("#enviarPreg").click(function(event)
+    {
+        
+        if(boolTextFiled == false || boolDropdown == false)
+        {
+            alert("Datos incorrectos");
+            event.preventDefault();
+        }
+
+        if (boolTextFiled == false) {
+            $(".pregunta").css({backgroundColor: 'red'});    
+        }
+        
         var dificultad = $("#dificultades").val();
         var tematica = $("#tematica").val();
         var tiempo = $("#tiempo").val();
+       
+        
+
         var espacioVacio = true;
+
+        if(boolTextFiled)
+        {
+            
+            espacioVacio = false;
+        }
+        if($(".radioPreg").attr("checked","true"))
+        {
+            espacioVacio = true; 
+        }else{
+            espacioVacio = false; 
+        }
         if(dificultad == -1)
         {
             espacioVacio = false;
-            alert("El campo Dificultad debe seleccionar uno.");
         }
         if(tematica == -1)
         {
             espacioVacio = false;
-            alert("El campo Dificultad debe seleccionar uno.");
         }
+        if(tiempo < 0)
+        {
+            espacioVacio = false;
+            tiempo.val() = 0;   
+        }
+        
+        
     });
     $("#contenido").keypress(function()
     {
-        var RestricionLetras = /(?=.*[\!@#$%&^()\\[\]{}\-_+=|:;"'<>,./?])(?=.*[a-z])(?=.*[A-Z])(?=(.*)).{8,}/;
+        var RestricionLetras = /(?=.*[\!@#$%&^()\\[\]{}\-_+=|:;"'<>,./?])(?=.*[a-z])(?=.*[A-Z])(?=(.*)).{2,}/;
         if(tiempo == RestricionLetras)
         {
             espacioVacio = false;
-            alert("Solo se puede introducir numeros.");
         }
+        
     });
+
 });
--- preguntas.php
+++ preguntas.php
@@ -19,26 +19,26 @@
     <h2 id="titulo">Agrega una nueva pregunta</h2>
     <form>
       <label for="pregunta">Pregunta:</label>
-      <input type="text" id="pregunta" name="pregunta" required>
+      <input type="text" id="pregunta" class="pregunta" name="pregunta">
       <label for="respuesta">Respuesta 1:</label>
-      <textarea id="respuesta" name="respuesta" required></textarea>
+      <textarea id="respuesta1" class="pregunta" name="respuesta1"></textarea>
       <label for="respuesta">Respuesta 2:</label>
-      <textarea id="respuesta" name="respuesta" required></textarea>
+      <textarea id="respuesta2" class="pregunta" name="respuesta2"></textarea>
       <label for="respuesta">Respuesta 3:</label>
-      <textarea id="respuesta" name="respuesta" required></textarea>
+      <textarea id="respuesta3" class="pregunta" name="respuesta3"></textarea>
       <label for="respuesta">Respuesta 4:</label>
-      <textarea id="respuesta" name="respuesta" required></textarea>
+      <textarea id="respuesta4" class="pregunta" name="respuesta4"></textarea>
       <div id="imagen">
         <label for="image">Imagen: </label>
-        <input type="file" title=" " required>
+        <input type="file" title=" " >
       </div>
       <div id="temporizador">
       <label for="tiempo">Segundos para resolver la pregunta:</label>
-      <input type="number" name="tiempo" id="tiempo" required>
+      <input type="number" name="tiempo" id="tiempo" >
       </div>
       <div class="dropdownDif">
         <label for="dificultades">Dificultades:</label>
-        <select name="dificultades" id="dificultades" required>
+        <select name="dificultades" id="dificultades" >
           <option value="-1">Seleccione un campo</option>
           <option value="1">Facil</option>
           <option value="2">Normal</option>
@@ -47,7 +47,7 @@
       </div>
         <div class="dropdownDif">
         <label for="tematica">Tematica:</label>
-        <select name="tematica" id="tematica" required>
+        <select name="tematica" id="tematica" >
           <option value="-1">Seleccione un campo</option>
           <option value="FOL">FOL</option>
           <option value="Ingles">Ingles</option>
@@ -56,15 +56,15 @@
       <label for="radios" id="labelRadios">Cual es la correcta:</label>
       <div id="radio">
       <label for="pregunta1">Pregunta 1:</label>
-      <input type="radio" id="" name="preguntaCor" value="" required>
+      <input type="radio" id="radio1" class = "radioPreg" name="preguntaCor" value="">
       <label for="pregunta2">Pregunta 2:</label>
-      <input type="radio" id="" name="preguntaCor" value="">
+      <input type="radio" id="radio2" class = "radioPreg" name="preguntaCor" value="">
       <label for="pregunta3">Pregunta 3:</label>
-      <input type="radio" id="" name="preguntaCor" value="">
+      <input type="radio" id="radio3" class = "radioPreg" name="preguntaCor" value="">
       <label for="pregunta4">Pregunta 4:</label>
-      <input type="radio" id="" name="preguntaCor" value="">
+      <input type="radio" id="radio4" class = "radioPreg" name="preguntaCor" value="">
       </div>
-      <input type="submit" value="Agregar pregunta">
+      <input type="submit" value="Agregar pregunta" id="enviarPreg">
     </form>
     </div>
     <script src="js/jquery-3.6.4.min.js"></script>
